524ddc21efb669a428f7bcc91f0e817b41f30d73
Deakristina <deakristina@gmail.com>
2018-12-11 14:54:58 +0100

    Add files via upload

--- models/jobVacancy.js
+++ models/jobVacancy.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 var Job = new Schema({
-  title: String,
-  duration: String,
-  description: String,
-  payment: Number,
-  requirements: Array,
-  host: [{type: Schema.ObjectId, ref:"User"}]
-})
+	title: String,
+	duration: String,
+	salary: Number,
+	description: String,
+	requirements: Array,
+	host: [ { type: Schema.ObjectId, ref: 'User' } ]
+});
 
-var job = mongoose.model("job", Job)
+var job = mongoose.model('job', Job);
 
-module.exports = job
+module.exports = job;
