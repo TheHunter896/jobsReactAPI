78f4aa05801b3d35da3c20ea8f2635ca492fb021
David Bros <david_corp896@outlook.es>
2018-12-17 11:42:10 +0100

    Hello there

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-require('dotenv').config();
-const local = require('./local');
-var cors = require('cors');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
-var app = express();
-const passport = require('passport');
+require('dotenv').config()
+const local = require('./local')
+var cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const favicon = require('serve-favicon')
+const mongoose = require('mongoose')
+const logger = require('morgan')
+const path = require('path')
+var app = express()
+const passport = require('passport')
 const bcrypt = require('bcrypt')
 const LocalStrategy = require('passport-local').Strategy
 const expressSession = require('express-session')
 
 
 // Middleware Setup
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.use(
 	cors({
 		origin: ['http://localhost:3000', "http://127.0.0.1:3000"],
 		credentials: true
 	})
-);
+)
 
 mongoose
 	.connect('mongodb://localhost/jobsAPI', { useNewUrlParser: true })
 	.then((x) => {
-		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
 	})
 	.catch((err) => {
-		console.error('Error connecting to mongo', err);
-	});
+		console.error('Error connecting to mongo', err)
+	})
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require('./package.json').name
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`)
 
 
 
@@ -49,44 +49,44 @@ passport.use(new LocalStrategy(
 	
     User.findOne({ "info.base.email": username }, function (err, user) {
 
-      if (err) { return done(err); }
-      else if (user === null) { return done(null, false); }
+      if (err) { return done(err) }
+      else if (user === null) { return done(null, false) }
 			else if (user != null){
 				bcrypt.compare(password, user.info.base.password, (err, res) => {
 					if(err){console.log(err); return done(null, false)}
 					else if(res){
 						debugger
-						 return done(null, user);
+						 return done(null, user)
 					}
 					else{
-						 return done(null, false);
+						 return done(null, false)
 					}
 				})
 			}
 			else{
 				debugger
-				 return done(null, false);
+				 return done(null, false)
 			}
-		});
+		})
 	})
-);
+)
 
 passport.serializeUser(function(user, done) {
 	debugger
- 	 done(null, {id: user.id, email: user.info.base.email});
-});
+ 	 done(null, {id: user.id, email: user.info.base.email})
+})
 
 passport.deserializeUser(function(id, done) {
 	debugger
   User.findById(id, function (err, user) {
 		debugger
-    done(err, user);
-  });
-});
+    done(err, user)
+  })
+})
 
 //Passport
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(expressSession({
 	secret: 'thelifeissomagnificentandexsitenceispain',
 	cookie: {
@@ -102,45 +102,45 @@ app.use(
 		dest: path.join(__dirname, 'public'),
 		sourceMap: true
 	})
-);
+)
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Express - Generated with IronGenerator'
 
-const index = require('./routes/index');
-const register = require('./routes/register.js');
-const postJob = require('./routes/postJob.js');
-const searchJob = require('./routes/searchJob.js');
-const getJob = require('./routes/getJob.js');
-const checkEmail = require('./routes/checkEmai.js');
-const login = require('./routes/login');
+const index = require('./routes/index')
+const register = require('./routes/register.js')
+const postJob = require('./routes/postJob.js')
+const searchJob = require('./routes/searchJob.js')
+const getJob = require('./routes/getJob.js')
+const checkEmail = require('./routes/checkEmai.js')
+const login = require('./routes/login')
 const profileInfo = require('./routes/profileInfo')
 const authentication = require('./routes/auth')
 
 //Routes
-app.use('/', index);
-app.use('/register', register);
-app.use('/post-job', postJob);
-app.use('/search-job', searchJob);
-app.use('/get-job', getJob);
-app.use('/checkEmail', checkEmail);
-app.use('/login', login);
+app.use('/', index)
+app.use('/register', register)
+app.use('/post-job', postJob)
+app.use('/search-job', searchJob)
+app.use('/get-job', getJob)
+app.use('/checkEmail', checkEmail)
+app.use('/login', login)
 app.use('/profileInfo', profileInfo)
 app.use('/auth', authentication)
 
-var os = require('os');
-var ifaces = os.networkInterfaces();
-console.log(ifaces);
+var os = require('os')
+var ifaces = os.networkInterfaces()
+console.log(ifaces)
 
-let ipAddress = local.ipAddress;
+let ipAddress = local.ipAddress
 
 app.listen(local.port, ipAddress, () => {
-	console.log(`Listening ${local.port}`);
-});
+	console.log(`Listening ${local.port}`)
+})
 
-module.exports = app;
+module.exports = app
--- routes/profileInfo.js
+++ routes/profileInfo.js
@@ -1,23 +1,12 @@
-// const express = require('express');
-// const router = express.Router();
-// // const checkAuthenticate = require('../public/javascripts/authenticate.js')
+const express = require('express');
+const router = express.Router();
+const checkAuthenticate = require('../public/javascripts/authenticate.js')
 
 var User = require('../models/User')
 
-// router.get('/', checkAuthenticate, (req, res) => {
-// 	console.log(req.body);
-// 	user
-// 		.findOne({ 'info.base.email': req.body })
-// 		.then((result) => {
-// 			console.log(result);
-// 			res.json(result);
-// 		})
-// 		.catch((err) => console.log(err));
-// });
-
 router.get('/', (req, res) => {
   debugger
-  User.findOne({"info.base.email": 'dvdbros@hotmail.com'})
+  User.findOne({"info.base.email":"dvdbros@hotmail.com"})
   .then((result) => {
     if(result == null){
       debugger
@@ -33,3 +22,4 @@ router.get('/', (req, res) => {
 })
 
 module.exports = router
+
